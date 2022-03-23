681287dad8af89e190d9145812c6f77bdd0c4879
Angel <angel.roldan@hola.com>
2022-03-23 18:47:44 +0100

    solved

--- src/typing.ts
+++ src/typing.ts
@@ -1,31 +1,40 @@
 // Funciones Básicas
-function sumar( a, b ){
+function cacafuti( a: number, b: number ): number {
   return a + b;
 }
 
-const contar = ( heroes ) => {
+const contar = ( heroes: string[] ) => {
   return heroes.length;
 }
 
 const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
 contar(superHeroes);
 
+
+// type tipoNada = (
+//   numero: string,
+//   text: string,
+//   booleano: boolean,
+//   arreglo: []
+// )
 // Tipo función
-const noHaceNada = ( numero, texto, booleano, arreglo )=> {}
+const noHaceNada = ( numero: number, texto: string, booleano: boolean, arreglo: [] )=> {}
 
 // Crear el tipo de función que acepte la función "noHaceNada"
 let noHaceNadaTampoco;
 noHaceNadaTampoco = noHaceNada
+// @TODO no sabemos cómo hacerlo
+// type noHaceNadaTampoco = noHaceNada 
 
 // Objetos
-const batmovil = {
+const batmovil: object = {
   carroceria: 'Negra',
   modelo: '6x6',
   antibalas: true,
   pasajeros: 4
 }
 
-const bumblebee = {
+const bumblebee: object = {
   carroceria: 'Amarillo con negro',
   modelo: '4x2',
   antibalas: true,
@@ -37,7 +46,7 @@ const bumblebee = {
 
 
 // Villanos debe de ser un arreglo de objetos personalizados
-const villanos = [{
+const villanos: object[] = [{
   nombre: 'Lex Luthor',
   edad: 54,
   mutante: false
@@ -53,18 +62,28 @@ const villanos = [{
 
 // Multiples tipos
 // cree dos tipos, uno para charles y otro para apocalipsis
-const charles = {
+type Charles = {
+  poder: string,
+  estatura: number,
+}
+
+const charles: Charles = {
   poder: 'psiquico',
   estatura: 1.78
 }
 
-const apocalipsis = {
+type Apocalipsis = {
+  lider: boolean,
+  miembros: number[]
+}
+
+const apocalipsis: Apocalipsis = {
   lider: true,
   miembros: ['Magneto', 'Tormenta', 'Psylocke', 'Angel']
 }
 
 // Mystique, debe poder ser cualquiera de esos dos mutantes (charles o apocalipsis)
-let mystique
+let mystique: Charles | Apocalipsis
 
 mystique = charles
 mystique = apocalipsis
